e90a728aee814b9c9351d077ea860ad9a72b39cb
elraro <elraro@elraro.eu>
2017-01-20 23:18:26 +0100

    Problema 372

---
+++ src/intervalo300/Problema372.java
@@ -0,0 +1,24 @@
+package intervalo300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problema372 {
+
+	static StringBuilder sb = new StringBuilder();
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int j = Integer.parseInt(in.readLine());
+		for (int i = 0; i < j; i++) {
+			char[] a = new StringBuilder(in.readLine()).reverse().toString().toCharArray();
+			if (Character.isUpperCase(a[a.length-1])) {
+				a[a.length-1] = Character.toLowerCase(a[a.length-1]);
+				a[0] = Character.toUpperCase(a[0]);
+			}
+			sb.append(a).append("\n");
+		}
+		System.out.print(sb.toString());
+	}
+}
