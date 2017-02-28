e2bbd4de8fbfe9ffd97ba67d2074f6432dba8c17
elraro <elraro@elraro.eu>
2017-02-28 23:16:43 +0100

    Problema 140

--- src/intervalo100/Problema140.java
+++ src/intervalo100/Problema140.java
@@ -3,33 +3,30 @@ package intervalo100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problema140 {
 
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 	static final String SALTO = "\n";
-	static final String SUM = " + ";
-	static final String EQ = " = ";
-	static String line;
+	static String[] line;
+	static int c;
+	static int p;
 	static int l;
-	static int n;
+	static int d;
+
+	static int t;
 
 	public static void main(String[] args) throws IOException {
-		while ((line = in.readLine()) != null) {
-			if (line.startsWith("-"))
-				break;
-			l = line.length();
-			n = 0;
-			for (int i = 0; i < l; i++) {
-				n += Character.getNumericValue(line.charAt(i));
-				sb.append(line.charAt(i));
-				if (i != l - 1)
-					sb.append(SUM);
-				else
-					sb.append(EQ).append(n).append(SALTO);
-			}
-
+		c = Integer.parseInt(in.readLine());
+		for (int i = 0; i < c; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			p = Integer.parseInt(st.nextToken());
+			l = Integer.parseInt(st.nextToken());
+			d = Integer.parseInt(st.nextToken());
+			sb.append(l * p - ((p * (p - 1) / 2) * d)).append(SALTO);
 		}
 		System.out.print(sb.toString());
 	}
