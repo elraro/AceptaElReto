86c85a5af4db7cb0840a53ac4b1886555af211c5
elraro <elraro@elraro.eu>
2017-03-01 11:44:58 +0100

    Ahora si

--- src/intervalo100/Problema140.java
+++ src/intervalo100/Problema140.java
@@ -3,29 +3,33 @@ package intervalo100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Problema140 {
 
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 	static final String SALTO = "\n";
-	static int c;
-	static int p;
+	static final String SUM = " + ";
+	static final String EQ = " = ";
+	static String line;
 	static int l;
-	static int d;
-
-	static int t;
+	static int n;
 
 	public static void main(String[] args) throws IOException {
-		c = Integer.parseInt(in.readLine());
-		for (int i = 0; i < c; i++) {
-			st = new StringTokenizer(in.readLine(), " ");
-			p = Integer.parseInt(st.nextToken());
-			l = Integer.parseInt(st.nextToken());
-			d = Integer.parseInt(st.nextToken());
-			sb.append(l * p - ((p * (p - 1) / 2) * d)).append(SALTO);
+		while ((line = in.readLine()) != null) {
+			if (line.startsWith("-"))
+				break;
+			l = line.length();
+			n = 0;
+			for (int i = 0; i < l; i++) {
+				n += Character.getNumericValue(line.charAt(i));
+				sb.append(line.charAt(i));
+				if (i != l - 1)
+					sb.append(SUM);
+				else
+					sb.append(EQ).append(n).append(SALTO);
+			}
+
 		}
 		System.out.print(sb.toString());
 	}
